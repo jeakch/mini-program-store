7945a7c4472201bcc290209298b13112813d7da0
jeakch <2976@qq.com>
2023-05-02 10:12:44 +0800

    project init

---
+++ yougou-shop/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>首页</view>
+</template>
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad () {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped></style>
